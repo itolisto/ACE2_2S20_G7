7b311c0dc22984e3dd38a84c53cb8f4efcdad5eb
Mauricio Flores <itolisto@gmail.com>
2020-08-17 20:23:04 -0600

    Fix magnetic and proximity sensor code

--- main/main.ino
+++ main/main.ino
@@ -1,35 +1,35 @@
 #include <HX711.h>
 
-const int dataPin=A1;
-const int CLK=A0;
+const int dataPin = A1;
+const int CLK = A0;
 double peso;
 
-const int trigPin = 9;
-const int echoPin = 10;
-long duration;
-int distance;
+const int trigPin = 4;
+const int echoPin = 5;
 
-const int switchPin = 2;
+const int magneticPin = 2;
 
 HX711 balanza;
 
 void setup() {
-  // Configurar data rate en baud (bits por segundo)
-  Serial.begin(9600);
   // Configurar switch magnético
-  pinMode(switchPin, INPUT);
+  pinMode(magneticPin, INPUT);
   // Configurar sensor de distancia
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  
   // Configurar sensor de peso
-  balanza.begin(DOUT, CLK);
-  Serial.print("Lectura del valor del ADC:t");
-  Serial.println(balanza.read());
-  Serial.println("No ponga ningún objeto sobre la balanza");
-  Serial.println("Destarando...");
-  balanza.set_scale(); //La escala por defecto es 1
-  balanza.tare(20);  //El peso actual es considerado Tara.
-  Serial.println("Coloque un peso conocido:");
+  //  balanza.begin(DOUT, CLK);
+  //  Serial.print("Lectura del valor del ADC:t");
+  //  Serial.println(balanza.read());
+  //  Serial.println("No ponga ningún objeto sobre la balanza");
+  //  Serial.println("Destarando...");
+  //  balanza.set_scale(); //La escala por defecto es 1
+  //  balanza.tare(20);  //El peso actual es considerado Tara.
+  //  Serial.println("Coloque un peso conocido:");
+  
+  // Configurar data rate en baud (bits por segundo)
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -44,31 +44,30 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
+  int duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
-  distance= duration*0.034/2;
+  int waterDistance = duration * 0.034 / 2;
   // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-
-  
+  Serial.println(waterDistance);
 
   //------------------------------------------------------
   // Obtener peso de balanza
   //------------------------------------------------------
-  Serial.print("Valor de lectura: t");
-  Serial.println(balanza.get_value(10),0);
-  delay(100);
+  int peso = 0;
+  //  Serial.print("Valor de lectura: t");
+  //  Serial.println(balanza.get_value(10),0);
+  //  delay(100);
 
   //------------------------------------------------------
   // Acciones
   //------------------------------------------------------
-
-  // 1. Si el switch está LOW, la puerta está cerrada
-  if (digitalRead(switchPin) == HIGH) {
+  // 1. Si el switch está HIGH, la puerta está cerrada
+  if (digitalRead(magneticPin) == HIGH) {
     // 2. Si hay peso
-    if (peso > 0.0) {
-      
-    }
+     Serial.println("Cerrada");
+  } else {
+     Serial.println("Abierto");
   }
+
+  delay(500);
 }
