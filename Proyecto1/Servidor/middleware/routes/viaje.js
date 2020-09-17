1392b5928d30c198826038add848eede911ee5ce
Jurtgen <2557346111221@ingenieria.usac.edu.gt>
2020-09-17 15:27:02 -0600

    promedios finalizados

--- Proyecto1/Servidor/middleware/routes/viaje.js
+++ Proyecto1/Servidor/middleware/routes/viaje.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 const viaje = require('../../rest-api-loopback/server/models/viaje');
-
-
+var moment = require('moment');
 
 router.post('/envio/salida', function(req, res, next) {
     // console.log(JSON.stringify(req.body));
@@ -19,13 +18,31 @@ router.post('/envio/salida', function(req, res, next) {
         data: viajeData
     })
     .then(function (response) {
-        res.status(200).json({ result: 'success' });
+   
+        let dataEstadista= {};
+        dataEstadista['idviaje']=response['data']['id'];
+        axios({ 
+            method:'post',
+            url:process.env.URL+'/api/estadisticas',
+            headers:{'Content-Type': 'application/json', 'Accept': 'application/json'},
+            data:dataEstadista
+        })
+        .then(function(response){
+                //debo a?adir un detalle viaje que va camino al cliente.
+            res.status(200).json({ result: 'success' });
+        })
+        .catch(function(error){
+            next(error);
+        });
+
+         
     })
     .catch(function (error) {
         console.log(viajeData);
-        //console.log(error);
+ 
         next(error);
     });
+
 });
 
 router.post('/envio/entrega', function(req, res, next) {
@@ -35,7 +52,7 @@ router.post('/envio/entrega', function(req, res, next) {
         viajeData['horaentrega']= new Date().toISOString();
 
       //  viajeData['horaentrega']='now';
-        const selectFilter = {"fields": {"id": true},"order": "timestamp DESC","limit":1};
+        const selectFilter = {"fields": {"id": true,"horasalida":true},"order": "timestamp DESC","limit":1};
         const url = process.env.URL + '/api/viajes?filter=' + encodeURIComponent(JSON.stringify(selectFilter));
         axios({
             method: 'get',
@@ -45,6 +62,13 @@ router.post('/envio/entrega', function(req, res, next) {
         .then(function (response) {
             // handle success
             let id = response.data[0]['id'];
+      
+            const  horasalida = moment(response.data[0]['horasalida']);
+            const horaentrega = moment(viajeData['horaentrega']);
+            const tiempoProm = horaentrega-horasalida;
+   
+
+            
              //res.status(200).json(response.data[0]);
 
              axios({
@@ -53,7 +77,43 @@ router.post('/envio/entrega', function(req, res, next) {
                 headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
                 data:viajeData
             }).then(function(response){
-                res.status(200).json({ result: 'success' });
+                    /**
+                        * dentro del patch para estadistica se debe enviar pesopromedio,tiempopromedioida
+                    */
+                   const selectFilter4 = {"fields": {"id": true},"order": "timestamp DESC","limit":1};
+                   const url4 = process.env.URL + '/api/estadisticas?filter=' + encodeURIComponent(JSON.stringify(selectFilter4));
+                   axios({
+                        method: 'get',
+                        url: url4,
+                        headers: {'Accept': 'application/json'},
+                    }).then(function(response4){
+
+                        let idest = response4.data[0]['id'];
+                        let dataEstadistico={}
+                        
+                        dataEstadistico['pesopromedio']=viajeData['pesoentrega'];
+                        dataEstadistico ['tiempopromedioida']  =tiempoProm;                     
+
+                        
+                        axios({
+                            method: 'patch',
+                            url: process.env.URL + '/api/estadisticas/'+idest,
+                            headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+                            data:dataEstadistico
+                        }).then(function(response5){
+
+  
+                                 res.status(200).json({ result: 'success' });
+
+                        }).catch(function(error){
+                            next(error);
+                        });
+
+
+                    }).catch(function(error){
+                        next(error);
+                    })
+
 
             }).catch(function (error){
                 next(error);
@@ -73,7 +133,7 @@ router.post('/envio/regreso', function(req, res, next) {
         viajeData['estadoviaje']='finalizado';
 
       //  viajeData['horaentrega']='now';
-        const selectFilter = {"fields": {"id": true},"order": "timestamp DESC","limit":1};
+        const selectFilter = {"fields": {"id": true,'horaentrega':true,'obstaculoentrega':true},"order": "timestamp DESC","limit":1};
         const url = process.env.URL + '/api/viajes?filter=' + encodeURIComponent(JSON.stringify(selectFilter));
         axios({
             method: 'get',
@@ -83,6 +143,10 @@ router.post('/envio/regreso', function(req, res, next) {
         .then(function (response) {
             // handle success
             let id = response.data[0]['id'];
+            let promedioRegreso = moment( viajeData['horaregreso'])-moment(response.data[0]['horaentrega']);
+            let totalObstaculos =viajeData['obstaculoregreso']+response.data[0]['obstaculoentrega'];
+
+           
              //res.status(200).json(response.data[0]);
 
              axios({
@@ -91,7 +155,41 @@ router.post('/envio/regreso', function(req, res, next) {
                 headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
                 data:viajeData
             }).then(function(response){
-                res.status(200).json({ result: 'success' });
+
+                //res.status(200).json({ result: 'success' });
+                const selectFilter4 = {"fields": {"id": true},"order": "timestamp DESC","limit":1};
+                const url4 = process.env.URL + '/api/estadisticas?filter=' + encodeURIComponent(JSON.stringify(selectFilter4));
+                axios({
+                     method: 'get',
+                     url: url4,
+                     headers: {'Accept': 'application/json'},
+                 }).then(function(response4){
+
+                     let idest = response4.data[0]['id'];
+                     let dataEstadistico={}
+                     
+                     dataEstadistico['totalobstaculos']=totalObstaculos;
+                     dataEstadistico ['tiempopromedioregreso']  =promedioRegreso;                     
+
+                     
+                     axios({
+                         method: 'patch',
+                         url: process.env.URL + '/api/estadisticas/'+idest,
+                         headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+                         data:dataEstadistico
+                     }).then(function(response5){
+
+                            console.log(dataEstadistico);
+                              res.status(200).json({ result: 'success' });
+
+                     }).catch(function(error){
+                         next(error);
+                     });
+
+
+                 }).catch(function(error){
+                     next(error);
+                 })
 
             }).catch(function (error){
                 next(error);
@@ -102,6 +200,7 @@ router.post('/envio/regreso', function(req, res, next) {
             next(error);
         });
 });
+
 
 
 
