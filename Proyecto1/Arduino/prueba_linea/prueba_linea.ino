be9544b816d4181bce9a9a363c5711374476bfd0
floresmau <2230563080101@ingenieria.usac.edu.gt>
2020-09-19 00:28:34 -0600

    Actualizar codigo de seguidor de linea de prueba
    Prueba exitosa

--- Proyecto1/Arduino/prueba_linea/prueba_linea.ino
+++ Proyecto1/Arduino/prueba_linea/prueba_linea.ino
@@ -3,6 +3,11 @@ const int FRONT2PIN = A2; // Izquierda
 const int FRONT1PIN = A3; // Derecha
 const int BACK1PIN = A4; // Izquierda
 const int BACK2PIN = A5; // Derecha
+
+const int frontLeftLimit = 300;
+const int frontRightLimit = 330;
+const int backLeftLimit = 450;
+const int backRightLimit = 430;
 // Pines de motores
 #define Motor11 7
 #define Motor12 6
@@ -12,13 +17,19 @@ const int BACK2PIN = A5; // Derecha
 #define PWMmotor2 10
 int valuePWM1 = 140; // Velocidad de motores cuando se mueven hacia el frente
 int valuePWM2 = 110; // Velocidad de motores cuando giran
+// Variables de configuración
+bool activo = false; // Si es falso no ejecuta ninguna accion, de lo contrario, camina.
 
-bool activo = false;
-int orientacion = 1;
+int orientacion = 1; // 1: Hacia adelante (ida) 2: Hacia atras (regreso)
 
 void setup() {
   // Configurar data rate en baud (bits por segundo)
   Serial.begin(115200);
+  // Configurar sensores de color
+  pinMode(FRONT1PIN, INPUT); // Sensor frontal derecho
+  pinMode(FRONT2PIN, INPUT); // Sensor frontal izquierdo
+  pinMode(BACK1PIN, INPUT); // Sensor trasero derecho
+  pinMode(BACK2PIN, INPUT); // Sensor trasero izquierdo
   // Configurar pines de motor
   pinMode(Motor11, OUTPUT);
   pinMode(Motor12, OUTPUT);
@@ -26,27 +37,15 @@ void setup() {
   pinMode(Motor22, OUTPUT);
   pinMode(PWMmotor1, OUTPUT);
   pinMode(PWMmotor2, OUTPUT);
-  // Configurar sensores de color
-  pinMode(FRONT1PIN, INPUT); // Sensor frontal derecho
-  pinMode(FRONT2PIN, INPUT); // Sensor frontal izquierdo
-  pinMode(BACK1PIN, INPUT); // Sensor trasero derecho
-  pinMode(BACK2PIN, INPUT); // Sensor trasero izquierdo
 }
 
 void loop() {
-  // 1. Leer consola serial para saber si debemos cambiar el estado de activo
-  // 2. Verificar estado de respuesta actual del carro, si es falso omitir cualquier accion
-  if(activo == false) {
+  if (activo == false) {
     activo = true;
-    Serial.println("Activando...");
     delay(10000);
-    Serial.println("Activo");
     return;
   }
-  //------------------------------------------------------
-  // Acciones
-  //------------------------------------------------------
-  // 4. Verificar orientacion actual
+  
   if (orientacion == 1) {
     // Desplazarse
     bool detenerse = desplazarse(orientacion);
@@ -68,24 +67,24 @@ void loop() {
 bool desplazarse(int orientacion) {
   int LEFT_SENSOR = 0;
   int RIGHT_SENSOR = 0;
+  int LEFT_LIMIT = 0;
+  int RIGHT_LIMIT = 0;
 
   if (orientacion == 1) {
     LEFT_SENSOR = analogRead(FRONT2PIN);
     RIGHT_SENSOR = analogRead(FRONT1PIN);
-    Serial.println("Front: " + String(LEFT_SENSOR) + String(",") + String(RIGHT_SENSOR));
+    LEFT_LIMIT = frontLeftLimit;
+    RIGHT_LIMIT = frontRightLimit;
   } else {
     LEFT_SENSOR = analogRead(BACK1PIN);
     RIGHT_SENSOR = analogRead(BACK2PIN);
-    Serial.println("Back: " + String(LEFT_SENSOR) + String(",") + String(RIGHT_SENSOR));
+    LEFT_LIMIT = backLeftLimit;
+    RIGHT_LIMIT = backRightLimit;
   }
-
-  
 
   bool detenerse = false;
 
-  int base_limit = 200;
-
-  if (RIGHT_SENSOR < base_limit && LEFT_SENSOR < base_limit) {
+  if (RIGHT_SENSOR < LEFT_LIMIT && LEFT_SENSOR < RIGHT_LIMIT) {
     // Adelante
     if (orientacion == 1) {
       digitalWrite(Motor11, HIGH);
@@ -100,7 +99,7 @@ bool desplazarse(int orientacion) {
     }
     analogWrite(PWMmotor1, valuePWM1);
     analogWrite(PWMmotor2, valuePWM1);
-  } else if (RIGHT_SENSOR > base_limit && LEFT_SENSOR < base_limit) {
+  } else if (RIGHT_SENSOR > RIGHT_LIMIT && LEFT_SENSOR < LEFT_LIMIT) {
     // Izquierda
     if (orientacion == 1) {
       digitalWrite(Motor11, LOW);
@@ -116,7 +115,7 @@ bool desplazarse(int orientacion) {
 
     analogWrite(PWMmotor1, valuePWM2);
     analogWrite(PWMmotor2, valuePWM2);
-  } else if (RIGHT_SENSOR < base_limit && LEFT_SENSOR > base_limit) {
+  } else if (RIGHT_SENSOR < RIGHT_LIMIT && LEFT_SENSOR > LEFT_LIMIT) {
     // Derecha
     if (orientacion == 1) {
       digitalWrite(Motor11, LOW);
@@ -132,7 +131,7 @@ bool desplazarse(int orientacion) {
 
     analogWrite(PWMmotor1, valuePWM2);
     analogWrite(PWMmotor2, valuePWM2);
-  } else if (RIGHT_SENSOR > base_limit && LEFT_SENSOR > base_limit) {
+  } else if (RIGHT_SENSOR > RIGHT_LIMIT && LEFT_SENSOR > LEFT_LIMIT) {
     // Detenerse
     detenerse = true;
     digitalWrite(Motor11, LOW);
