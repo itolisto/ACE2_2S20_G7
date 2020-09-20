8d7c7e674a2ede30c0bac1b3f464df3d5f8fe581
Jurtgen <2557346111221@ingenieria.usac.edu.gt>
2020-09-20 17:35:59 -0600

    mejorando codigo

--- Proyecto1/Servidor/middleware/routes/vehiculo.js
+++ Proyecto1/Servidor/middleware/routes/vehiculo.js
@@ -1,56 +1,62 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
-const viaje = require('../../rest-api-loopback/server/models/detalleviaje');
-
 
 router.post('/vehiculo/progreso', function(req, res, next) {
-    // console.log(JSON.stringify(req.body));
-
-        let vehiculoData = {};
-        vehiculoData['estado'] = req.body.estado;
-
-
-        if(req.body.estado=='reposo'){
-            vehiculoData['ubicacion']='buzon';
-        }else{
-            vehiculoData['ubicacion']='cliente';
-        }
-        const selectFilter = {"fields": {"id": true},"order": "timestamp DESC","limit":1};
-        const url = process.env.URL + '/api/viajes?filter=' + encodeURIComponent(JSON.stringify(selectFilter));
-        axios({
-            method: 'get',
-            url: url,
-            headers: {'Accept': 'application/json'},
-        })
-        .then(function (response) {
-            // handle success
-            vehiculoData['idviaje']= response.data[0]['id'];
-            console.log('identificador'+vehiculoData['id']);
-             //res.status(200).json(response.data[0]);
-
-            axios({
-                method: 'post',
-                url: process.env.URL + '/api/detalleviajes',
-                headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
-                data: vehiculoData
-            })
-            .then(function (response) {
-                res.status(200).json({ result: 'success' });
-            })
-            .catch(function (error) {
-                console.log(vehiculoData);
-                //console.log(error);
-                next(error);
-            });
-       
-        }).catch(function (error) {
-
-            next(error);
-        }); 
+    let vehiculoData = {};
+    vehiculoData['estado'] = req.body.estado;
+    if(req.body.estado=='reposo'){
+        vehiculoData['ubicacion']='buzon';
+    }else{
+        vehiculoData['ubicacion']='cliente';
+    }
+    const selectFilter = {"fields": {"id": true},"order": "timestamp DESC","limit":1};
+    const url = process.env.URL + '/api/viajes?filter=' + encodeURIComponent(JSON.stringify(selectFilter));
+    axios({
+        method: 'get',
+        url: url,
+        headers: {'Accept': 'application/json'},
+    })
+    .then(function (response) {
+        vehiculoData['idviaje']= response.data[0]['id'];
+        return axios({
+            method: 'post',
+            url: process.env.URL + '/api/detalleviajes',
+            headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+            data: vehiculoData
+        });
+    }).then(function () {
+        res.status(200).json({ result: 'success' });
+    }).catch(function (error) {
+        next(error);
+    }); 
        
   
 });
 
 
+router.post('/vehiculo/modo', function(req, res, next) {
+    const selectFilter = {"fields": {"id": true},"order": "timestamp DESC","limit":1};
+    const url = process.env.URL + '/api/viajes?filter=' + encodeURIComponent(JSON.stringify(selectFilter));
+  
+    axios({
+        method: 'get',
+        url: url,
+        headers: {'Accept': 'application/json'},
+    }).then(function (response) {
+    
+        let viajeData={
+            modo:body.req.modo
+        };
+        return axios({
+            method: 'patch',
+            url: process.env.URL + '/api/viajes/'+response.data[0]['id'],
+            headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+            data:viajeData
+        });
+    }).catch(function (error) {
+        next(error);
+    }); ;
+
+});
 module.exports = router;
