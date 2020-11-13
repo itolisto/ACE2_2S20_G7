734820342029ae943d0d0c43addf8b9ada5676bf
Jurgen Ramirez <2557346111221@ingenieria.usac.edu.gt>
2020-11-13 01:36:33 -0600

    cambios

--- Proyecto 2/Servidor/middleware/routes/safebox.js
+++ Proyecto 2/Servidor/middleware/routes/safebox.js
@@ -26,24 +26,55 @@ router.post('/safebox/llegada',function(req,res,netxt){
 
 });
 
-router.post('/safebox/dashboard',function(req,res,netxt){
+router.get('/safebox/dashboard',function(req,res,netxt){
     
-
-    let contador=0;
+    let dashboardData ={   };
+ 
 
     axios({
         method: 'get',
         url: process.env.URL + '/api/safeboxes/count',
         headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
     }).then(function(response){
-       contador = response.data['count'];
+        dashboardData['contador'] = response.data['count'];
+        const selectFilter1 = {"fields": {"acceso": true}, "where": {"acceso": {"gte": "1"}}};
+        const url1 = process.env.URL + '/api/safeboxes?filter=' + encodeURIComponent(JSON.stringify(selectFilter1));
         return axios({
             method:'get',
-            url: process.env.URL + '/api/viajes/count',
+            url: url1,
             headers: {'Accept': 'application/json'},
         })
+    }).then(function(response){
+
+        dashboardData['entradas'] = response.data.length;
+
+        const selectFilter2 = {"fields": {"acceso": true}, "where": {"acceso": {"lte": "0"}}};
+        const url2 = process.env.URL + '/api/safeboxes?filter=' + encodeURIComponent(JSON.stringify(selectFilter2));
+        return axios({
+            method:'get',
+            url: url2,
+            headers: {'Accept': 'application/json'},
+        })
+    }).then(function(response){
+        dashboardData['denegados'] = response.data.length;
+        const selectFilter3 = {"fields": {"temperatura": true,"order": "timestamp DESC"}};
+        const url3 = process.env.URL + '/api/safeboxes?filter=' + encodeURIComponent(JSON.stringify(selectFilter3));
+        return axios({
+            method:'get',
+            url: url3,
+            headers: {'Accept': 'application/json'},
+        })
+    }).then(function(response){
+        console.log(response.data);
+       let total = response.data.reduce(function(tot,element){
+            return tot+element.temperatura;
+       },0);
+       console.log(total);
+       dashboardData['promedio']= total/ dashboardData['contador'];
+        res.status(200).json(dashboardData);
     }).catch(function (error) {
         next(error);
+        console('error');
     });
     
 
